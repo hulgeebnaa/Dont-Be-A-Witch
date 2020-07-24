b1f8b7aedf56f0beae6d9af2a2ee55b1d1d673fd
Hulgee <it18d011@erdenetis.edu.mn>
2020-07-24 15:32:05 +0800

    Shoo shideed bolihod shoo alga boldog bolov

--- app.js
+++ app.js
@@ -8,9 +8,22 @@ var currentScore = 0;
 var sideOfDice = 0;
 // #- ni id gaar ni handaj bui
 window.document.querySelector(".btn-roll").addEventListener("click", RollDice);
+document.querySelector(".btn-new").addEventListener("click", newGame);
+var dicePng = document.querySelector(".dice");
+dicePng.style.display = "none";
 function RollDice() {
   sideOfDice = Math.floor(Math.random() * 6 + 1);
-  window.document.querySelector("#score-0").textContent = sideOfDice;
-  window.document.getElementById("score-1").textContent = sideOfDice; // iluu hurdan ajildag
-  document.querySelector(".dice").src = "dice-" + sideOfDice + ".png";
+  window.document.getElementById("current-0").textContent = sideOfDice;
+  window.document.getElementById("current-1").textContent = sideOfDice; // iluu hurdan ajildag
+  dicePng.style.display = "block";
+  dicePng.src = "dice-" + sideOfDice + ".png";
+}
+function newGame() {
+  dicePng.style.display = "none";
+  document.querySelector(".dice").src = window.document.getElementById(
+    "score-0"
+  ).textContent = "0";
+  document.getElementById("score-1").textContent = "0";
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
 }
